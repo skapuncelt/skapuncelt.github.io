02bc288e6e45bdf3b93490816162443f75d7d0bc
skapuncelt <spc.drumer@gmail.com>
2026-09-11 03:14:48 +0900

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,276 @@
-console.log("SkaPunCelt QUIZ START!");
+const quizQuestions = [
+    {
+        question: "SkaPunCeltの読み方は？",
+        choices: [
+            "スカパンケルト",
+            "スカプンセルト",
+            "スカパンセルト",
+            "スカプンケルト"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCeltの「Celt」の音楽的ルーツとして、特に深い関わりがある国はどこでしょう？",
+        choices: [
+            "アイルランド",
+            "ジャマイカ",
+            "アメリカ",
+            "エジプト"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "ハリーダックちゃんを生み出したのは誰でしょう？",
+        choices: [
+            "のすけ",
+            "ひろと",
+            "しんや",
+            "神"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCelt初めての曲の曲名は？",
+        choices: [
+            "ミャオミャオ",
+            "Skadan!",
+            "Lifeline",
+            "Ride The Wave!!🏄‍♂️"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "ライドザウェーブの正しい表記は？",
+        choices: [
+            "Ride The Wave!!🏄‍♂️",
+            "Ride the wave",
+            "Ride The Wave!🏄‍♂️",
+            "Ride The Wave!!"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "初めてハリーダックちゃんが出現したライブはどこ？",
+        choices: [
+            "神戸KINGSX",
+            "心斎橋新神楽",
+            "アメリカ村BEYOND",
+            "心斎橋VARON"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCeltの衣装はどこのブランドで揃えているでしょう？",
+        choices: [
+            "BILLABONG",
+            "そろえていない",
+            "YONEX",
+            "mizuno"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "BILLABONGと出会ったことでできた曲は？",
+        choices: [
+            "Ride The Wave!!🏄‍♂️",
+            "Skadan!",
+            "Future plan",
+            "Believe in yourself"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "ひろとのバンドでの役割は？",
+        choices: [
+            "外交大臣",
+            "物販大臣",
+            "デジタル大臣",
+            "総合的なことをしている"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "Skadan!の初めの掛け声は？",
+        choices: [
+            "Can you Skadan? Yes,I can!",
+            "おどれますか～！　は～い！",
+            "1234(ワンツースリーフォー)",
+            "掛け声などない"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "PartnerのAメロでセーラは繰り返し何と歌っている？",
+        choices: [
+            "Yah!Hello!!",
+            "Eee-oh",
+            "wow!! yeah!!",
+            "歌っていない"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCeltの結成年は？",
+        choices: [
+            "2024年",
+            "2025年",
+            "2020年",
+            "2014年"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCeltの物販で売っているガチャガチャに入っているものは？",
+        choices: [
+            "缶バッチ",
+            "ステッカー",
+            "ラバーバンド",
+            "ガチャガチャはない"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCeltの今までのライブで使用したことのない楽器は？",
+        choices: [
+            "タンバリン",
+            "スライドホイッスル",
+            "ティンホイッスル",
+            "サックス"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "ミナホニューエイジファイナルに出演するにあたりボイトレに行ったSkaPunCeltですが、ボイトレに行ったことでPartnerのAメロで変化がありました。それは何？",
+        choices: [
+            "「やぁ ハロー」から「イェア ヘロー」になった",
+            "歌う人が変わった",
+            "全員で歌うことになった",
+            "リズムが変わった"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCeltのライブで「Can you Skadan?」と言われたら？",
+        choices: [
+            "Yes,I can!",
+            "No,I can't",
+            "Maybe……",
+            "グァ！"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCelt初の曲「ミャオミャオ」が幻の曲になった理由は？",
+        choices: [
+            "歌詞が完成しなかったから",
+            "曲が長すぎたから",
+            "曲が難しすぎたから",
+            "猫よりアヒルが好きだから"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCeltの初ライブでは、セットリストに何が多かった？",
+        choices: [
+            "インスト曲",
+            "カバー曲",
+            "アカペラ曲",
+            "ソロ曲"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCeltの公式キャラクターの名前は？",
+        choices: [
+            "ハリーダック",
+            "スカパンマン",
+            "ケルトくん",
+            "パンクちゃん"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "Deadly Driveは名前の通りとても速い曲ですがBPMはどのぐらいでしょう？",
+        choices: [
+            "240",
+            "200",
+            "300",
+            "190"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "Deadly Driveの歌詞で「Head bob if you」のあとの掛け声は？",
+        choices: [
+            "Don't wanna die!",
+            "Yes,I can!",
+            "are not tired!",
+            "can dance!"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "初ミーティングで決めた、SkaPunCeltの大きな目標は？",
+        choices: [
+            "MINAMI WHEELに出演する",
+            "日本武道館でワンマンする",
+            "CDを1000枚売る",
+            "海外ツアーをする"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "MINAMI WHEEL -New Age- FINALに向けて、SkaPunCeltが行ったことは？",
+        choices: [
+            "ボイトレ",
+            "ダンスレッスン",
+            "水泳教室",
+            "滝行"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCeltのメンバーが知り合ったきっかけは？",
+        choices: [
+            "同じ学校の軽音楽部",
+            "ネットのメンバー募集",
+            "地元のスタジオ",
+            "ゲームのネッ友"
+        ],
+        answer: 0
+    },
+
+    {
+        question: "SkaPunCeltのロゴでないものは？",
+        choices: [
+            "アヒルロゴ",
+            "ストファイロゴ",
+            "黄緑ケルトロゴ",
+            "標識ロゴ"
+        ],
+        answer: 0
+    }
+];
